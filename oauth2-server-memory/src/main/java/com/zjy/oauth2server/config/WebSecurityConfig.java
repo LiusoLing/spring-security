4e2455c9c5f4e34abf440c8efc560b1d4ac4dfff
刘根来 <15879144378@163.com>
2021-07-21 14:33:43 +0800

    add oauth2 memory 版本提交，初步了解oauth2

---
+++ oauth2-server-memory/src/main/java/com/zjy/oauth2server/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.zjy.oauth2server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author liugenlai
+ * @since 2021/7/21 14:11
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
